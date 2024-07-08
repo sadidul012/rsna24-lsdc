2c45f96ef5a059fcefc06d827f15944f1dc0fcbd
Sadidul Islam <sadidul.012@gmail.com>
2024-07-08 12:13:53 +0600

    Implemented model config in training script

---
+++ config.py
@@ -0,0 +1,30 @@
+import json
+
+
+class ModelConfig:
+    MODEL_PATH = 'rsna24-data/models_db/xception41-DB-c3p1b16e20f14/'
+    MODEL_FILENAME = 'axial_t2-best_wll_model_fold-0.pt'
+    MODEL_NAME = "xception41"
+    N_LABELS = 25
+
+    IMG_SIZE = [512, 512]
+    IN_CHANS = 3
+
+    def __init__(self):
+        pass
+
+    def load(self, path):
+        with open(path, 'r') as f:
+            config = json.load(f)
+            self.__dict__.update(config)
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            json.dump(self.__dict__, f, indent=4)
+
+
+if __name__ == '__main__':
+    c = ModelConfig()
+    c.load('config.json')
+    print(c.MODEL)
+    print(c.IMAGE_SIZE)
--- single_train.py
+++ single_train.py
@@ -14,6 +14,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 from transformers import get_cosine_schedule_with_warmup
 
+from config import ModelConfig
 from single_inference import RSNA24Model
 from single_dataset import read_train_csv, DATA_PATH, process_train_csv, RSNA24DatasetBase, train_transform, balance_dataset
 
@@ -63,45 +64,43 @@ from single_dataset import read_train_csv, DATA_PATH, process_train_csv, RSNA24D
 # timm/tinynet_e.in1k - 2M - 0.765 - DB 0.811
 
 
-MODEL_NAME = "xception41"
-
 rd = '/mnt/Cache/rsna-2024-lumbar-spine-degenerative-classification'
-DEBUG = False
 
+DEBUG = False
 PRETRAINED = True
 RETRAIN = False
-
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 USE_AMP = True  # can change True if using T4 or newer than Ampere
-
 N_WORKERS = 4
 SEED = 8620
+
 GRAD_ACC = 2
 TGT_BATCH_SIZE = 32
 BATCH_SIZE = TGT_BATCH_SIZE // GRAD_ACC
 MAX_GRAD_NORM = None
 EARLY_STOPPING_EPOCH = 3
 
-IMG_SIZE = [512, 512]
-IN_CHANS = 3
-
 AUG_PROB = 0.75
 N_FOLDS = 14 if not DEBUG else 2
-EPOCHS = 20 if not DEBUG else 2
-
+EPOCHS = 2 if not DEBUG else 2
 LR = 1e-2
 WD = 1e-2
 AUG = True
 
-MODEL_SLUG = F"DB-c{IN_CHANS}p{1 if PRETRAINED else 0}b{BATCH_SIZE}e{EPOCHS}f{N_FOLDS}"
+model_config = ModelConfig()
+model_config.IMG_SIZE = [512, 512]
+model_config.IN_CHANS = 3
+model_config.MODEL_NAME = "xception41"
+
+MODEL_SLUG = F"DB-c{model_config.IN_CHANS}p{1 if PRETRAINED else 0}b{BATCH_SIZE}e{EPOCHS}f{N_FOLDS}"
 try:
-    MODEL_SLUG = f"{MODEL_NAME.split("/")[1]}-{MODEL_SLUG}"
+    MODEL_SLUG = f"{model_config.MODEL_NAME.split("/")[1]}-{MODEL_SLUG}"
 except IndexError:
-    MODEL_SLUG = f"{MODEL_NAME}-{MODEL_SLUG}"
+    MODEL_SLUG = f"{model_config.MODEL_NAME}-{MODEL_SLUG}"
 
 OUTPUT_FOLDER = "rsna24-data"
-OUTPUT_DIR = f'{OUTPUT_FOLDER}/models_db/{MODEL_SLUG}'
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+model_config.MODEL_PATH = f'{OUTPUT_FOLDER}/models_db/{MODEL_SLUG}'
+os.makedirs(model_config.MODEL_PATH, exist_ok=True)
 
 
 def set_random_seed(seed: int = 8620, deterministic: bool = False):
@@ -123,7 +122,7 @@ scaler = torch.cuda.amp.GradScaler(enabled=USE_AMP, init_scale=4096)
 
 def train(df, df_balanced, plane, n_classes):
     skf = KFold(n_splits=N_FOLDS, shuffle=True, random_state=SEED)
-    n_labels = int(n_classes / 3)
+    model_config.N_LABELS = int(n_classes / 3)
     fold_score = []
     study_ids = np.array(df.study_id.unique())
 
@@ -144,9 +143,9 @@ def train(df, df_balanced, plane, n_classes):
             valid_ds, batch_size=BATCH_SIZE * 2, shuffle=False, pin_memory=False, drop_last=False, num_workers=N_WORKERS
         )
 
-        model = RSNA24Model(MODEL_NAME, n_classes=n_classes, pretrained=PRETRAINED)
+        model = RSNA24Model(model_config.MODEL_NAME, n_classes=n_classes, pretrained=PRETRAINED)
         if RETRAIN:
-            fname = f'{OUTPUT_DIR}/{plane}-best_wll_model_fold-{fold}.pt'
+            fname = f'{model_config.MODEL_PATH}/{plane}-best_wll_model_fold-{fold}.pt'
             model.load_state_dict(torch.load(fname))
         model.to(device)
         optimizer = AdamW(model.parameters(), lr=LR, weight_decay=WD)
@@ -161,7 +160,7 @@ def train(df, df_balanced, plane, n_classes):
             num_cycles=num_cycles
         )
 
-        weights = torch.tensor([1.0, 2.0, 4.0] * n_labels)
+        weights = torch.tensor([1.0, 2.0, 4.0] * model_config.N_LABELS)
         criterion = nn.CrossEntropyLoss(weight=weights.to(device))
         criterion2 = nn.CrossEntropyLoss(weight=weights)
 
@@ -182,7 +181,7 @@ def train(df, df_balanced, plane, n_classes):
                     with autocast:
                         loss = 0
                         y = model(x)
-                        loss = loss + criterion(y, t) / n_labels
+                        loss = loss + criterion(y, t) / model_config.N_LABELS
                         total_loss += loss.item()
                         if GRAD_ACC > 1:
                             loss = loss / GRAD_ACC
@@ -225,7 +224,7 @@ def train(df, df_balanced, plane, n_classes):
                         with autocast:
                             loss = 0
                             y = model(x)
-                            loss = loss + criterion(y, t) / n_labels
+                            loss = loss + criterion(y, t) / model_config.N_LABELS
                             y_preds.append(y.cpu())
                             labels.append(t.cpu())
                             total_loss += loss.item()
@@ -234,7 +233,7 @@ def train(df, df_balanced, plane, n_classes):
 
                 y_preds = torch.cat(y_preds, dim=0)
                 labels = torch.cat(labels)
-                val_wll = criterion2(y_preds, labels) / n_labels
+                val_wll = criterion2(y_preds, labels) / model_config.N_LABELS
                 old_val_loss = best_loss
                 old_wll_metric = best_wll
                 if val_loss < best_loss or val_wll < best_wll:
@@ -249,8 +248,10 @@ def train(df, df_balanced, plane, n_classes):
                     if val_wll < best_wll:
                         # print(f'epoch:{epoch}, best wll_metric updated from {best_wll:.6f} to {val_wll:.6f}')
                         best_wll = val_wll
-                        fname = f'{OUTPUT_DIR}/{plane}-best_wll_model_fold-{fold}.pt'
-                        torch.save(model.state_dict(), fname)
+                        name = f'{plane}-best_wll_model_fold-{fold}'
+                        model_config.MODEL_FILENAME = name
+                        torch.save(model.state_dict(), f"{model_config.MODEL_PATH}/{name}.pt")
+                        model_config.save(f"{model_config.MODEL_PATH}/{name}.json")
 
                     if device != 'cuda:0':
                         model.to(device)
